25b8b0b8c7c2ca5df34a938c7c1ec8a3317c65bd
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-08 20:39:31 +0330

    # 03 - Average

---
+++ 04 - average/js/scripts.js
@@ -0,0 +1,6 @@
+let num1 = +prompt("Please Enter Number 1 : ")
+let num2 = +prompt("Please Enter Number 2 : ")
+let num3 = +prompt("Please Enter Number 3 : ")
+
+let ave = (num1 + num2 + num3) / 3
+console.log("average is : ", ave)
