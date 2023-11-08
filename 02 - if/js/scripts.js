02b506aa21c49ab40d5ea9fb01c99d507f79efc4
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-08 20:17:03 +0330

    # 02 - if - odd & even

--- 02 - if/js/scripts.js
+++ 02 - if/js/scripts.js
@@ -1,10 +1,11 @@
 let num = +prompt("Enter a Number : ");
-if (num % 2 == 0) {
-    console.log("Number is Even");
-}
 if (num == 0) {
     console.log("Number is Zero");
 }
+else if (num % 2 == 0) {
+    console.log("Number is Even");
+}
+
 else {
     console.log("Number is Odd");
 }
