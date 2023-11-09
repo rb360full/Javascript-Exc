341e1580ae77ed4c9e0d940e4e3503b7b5f98c99
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-09 12:36:36 +0330

    #

--- _Temp/js/scripts.js
+++ _Temp/js/scripts.js
@@ -1,8 +1,6 @@
-console.log(3==4);
-console.log(3===4);
-console.log(3!=4);
-console.log(3!==4);
-console.log(3>4);
-console.log(3>=4);
-console.log(3<4);
-console.log(3<=4);
+let num = +prompt("Please Enter Your Number : ", 10);
+let allowAcces = (num > 10) ? "greater than 10" :
+    (num < 10) ? "less than 10" :
+        "equal 10";
+        
+console.log(allowAcces)
