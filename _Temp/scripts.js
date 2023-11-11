88b6e81ef0dd6de08decbc553a00d8509aa960ab
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-11 18:53:53 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,4 +1,17 @@
-var sayHello = function () {
-    console.log("Hello World");
-};
-sayHello();
+let num1 = +prompt("Enter Number 1 : ")
+let num2 = +prompt("Enter Number 2 : ")
+
+
+
+console.log(typeof num1, typeof num2)
+console.log(num1, num2)
+
+function Sum(a = 0, b = 0) {
+    if (isNaN(a) || isNaN(b)) {
+        return "Please enter a valid number"
+    }
+    return a + b;
+}
+
+
+alert(Sum(num1, num2))
