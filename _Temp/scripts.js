1d792b403c6b65b799262433449bdc4033105743
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-12 10:09:51 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,12 +1,1 @@
-a = 1
-b = 10
-let check = (a, b) => {
-    var result;
-    a == b ? result = "equal" :
-        a + b > 5 ? result = ">5" :
-            a + b < 5 ? result = "<5" :
-                result = a + b;
-    return result;
-}
-
-console.log(check(a, b))
+console.log("Reza".includes("d")); //
