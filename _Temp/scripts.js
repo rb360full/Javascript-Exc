33f95e99b6367f933f981bf082f73c31b65235d9
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-12 08:20:29 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,17 +1,12 @@
-let num1 = +prompt("Enter Number 1 : ")
-let num2 = +prompt("Enter Number 2 : ")
-
-
-
-console.log(typeof num1, typeof num2)
-console.log(num1, num2)
-
-function Sum(a = 0, b = 0) {
-    if (isNaN(a) || isNaN(b)) {
-        return "Please enter a valid number"
-    }
-    return a + b;
+a = 1
+b = 10
+let check = (a, b) => {
+    var result;
+    a == b ? result = "equal" :
+        a + b > 5 ? result = ">5" :
+            a + b < 5 ? result = "<5" :
+                result = a + b;
+    return result;
 }
 
-
-alert(Sum(num1, num2))
+console.log(check(a, b))
