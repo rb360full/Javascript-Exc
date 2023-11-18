68fbc516a4b87f266a75c6568b575bb2ca842ade
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-18 15:54:42 +0330

    # 29 - Online Store

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,9 +1,15 @@
-let name={
-    name:"Reza",
-    age:25,
-    city:"Tehran",
-    country:"Iran",
+let users = [
+    { name: 'ali', age: 20 },
+    { name: 'reza', age: 30 },
+    { name: 'mohammad', age: 40 }
+]
 
-}
-console.log(name["age"]);
-console.log(name.age);
+let isInUsers = users.some(function () {
+    return user.name === 'ali';
+})
+
+// let isInUsers = users.some((user) => user.name === 'ali'); // arrow function
+
+console.log(isInUsers);
+
+
