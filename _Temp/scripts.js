8e98ece074d6d30d2d115f69fc6cd81f6a8445be
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-19 15:30:23 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,8 +1,8 @@
-let scores = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-let ismoreThan15 = scores.every(function (score) {
-
-    console.log(score);
-    return score < 15
+var names = ["reza", "sara", "ali", "mohammad", 'mohsen', 'akbar'];
+var userIndex =  names.findIndex(function (name) {
+    return name === 'mohsen';
 })
-console.log(ismoreThan15);
+console.log(userIndex); // Output : 4
+
+
+
