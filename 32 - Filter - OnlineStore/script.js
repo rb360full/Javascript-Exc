e6d1787566e582157e3e6dea39a194c224b20703
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-19 20:26:16 +0330

    # 32 - Filter - OnlineStore

--- 32 - Filter - OnlineStore/script.js
+++ 32 - Filter - OnlineStore/script.js
@@ -3,13 +3,13 @@ let cart = [
     { id: 2, name: "Mouse", price: 80000 },
     { id: 3, name: "Keyboard", price: 90000 },
     { id: 4, name: "Monitor", price: 250000 },
-    { id: 5, name: "CPU", price: 300000 },
+    { id: 5, name: "CPU", price: 99000 },
     { id: 6, name: "RAM", price: 60000 },
 ];
 
 let totalSum = 0;
 let invoice = cart.map(function (product) {
-    if (product.price > 100000) {
+    if (product.price < 100000) {
         return product.price + 1000;
     } else return product.price;
 });
@@ -18,4 +18,4 @@ invoice.forEach(function (param) {
     totalSum += param;
 });
 
-console.log('totalSum = ' , totalSum);
+console.log('totalSum = ', totalSum);
