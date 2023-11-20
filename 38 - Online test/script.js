4fe9ee19810bc79973d7776cb6197033038e1dd9
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-20 20:49:44 +0330

    3

---
+++ 38 - Online test/script.js
@@ -0,0 +1,7 @@
+let ostan = {
+    fars: ['shiraz', 'jahrom', 'kazeroon', 'lar', 'fasa'],
+    khoozestan: ['abadan', 'khoramshahr', 'ahvaz', 'dezful', 'behbehan']
+}
+
+let userOstan = prompt("Enter your Ostan : ").toString();
+console.log(ostan[userOstan]);
--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,12 +1,25 @@
+let num1 = 10
+let num2 = num1
+
+num2 = 20;
+
+console.log('num1:', num1); // 10
+console.log('num2:', num2); // 20
 
 
-let product1 = { name: 'mobile', price: 500 }
 
-product2 = product1;
 
-product2.color = 'red';
 
-console.log(product1); // { name: 'mobile', price: 500, color: 'red' }
-console.log(product2); // { name: 'mobile', price: 500, color: 'red' }
+let nums1 = [1,2,3,4]
+let nums2 = nums1
+
+num2=[5,6,7,8]
+nums2.push(5)
+
+console.log('nums1:', nums1); // 1 , 2 , 3 , 4
+console.log('nums2:', nums2); // 5 , 6 , 7 , 8
+
+
+
 
 
