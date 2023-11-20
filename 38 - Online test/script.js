70e7c41155522e706ef0475e5364c68042e74c52
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-20 21:20:07 +0330

    # 38 - Online test

--- 38 - Online test/script.js
+++ 38 - Online test/script.js
@@ -1,7 +1,22 @@
-let ostan = {
-    fars: ['shiraz', 'jahrom', 'kazeroon', 'lar', 'fasa'],
-    khoozestan: ['abadan', 'khoramshahr', 'ahvaz', 'dezful', 'behbehan']
-}
+let questions = [
+    { question: "what is the type of water?", answer: "liquid" },
+    { question: "what is the type of stone?", answer: "solid" },
+    { question: "what is the type of air?", answer: "gas" },
+];
 
-let userOstan = prompt("Enter your Ostan : ").toString();
-console.log(ostan[userOstan]);
+let userScores = 0;
+let userQuestion;
+let userAnswer;
+
+questions.forEach(function (question) {
+    userQuestion = question.question;
+    userAnswer = prompt(userQuestion + "\nliquid \nsolid \ngas");
+
+    questions.forEach(function (question) {
+        if (question.question == userQuestion && question.answer == userAnswer) {
+            userScores++;
+        }
+    });
+});
+
+alert("Your Scores is : " + userScores);
