af814a4f9bcb42bd3d41b9b3f892db15d4587623
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-20 22:04:22 +0330

    # 38 - Online test

--- 38 - Online test/script.js
+++ 38 - Online test/script.js
@@ -2,6 +2,10 @@ let questions = [
     { question: "what is the type of water?", answer: "liquid" },
     { question: "what is the type of stone?", answer: "solid" },
     { question: "what is the type of air?", answer: "gas" },
+    { question: "2 + 3", answer: "5" },
+    { question: "7 - 4", answer: "3" },
+    { question: "2 * 6", answer: "12" },
+    { question: "5 * 3", answer: "15" }
 ];
 
 let userScores = 0;
@@ -10,11 +14,12 @@ let userAnswer;
 
 questions.forEach(function (question) {
     userQuestion = question.question;
-    userAnswer = prompt(userQuestion + "\nliquid \nsolid \ngas");
+    userAnswer = prompt(userQuestion);
 
     questions.forEach(function (question) {
-        if (question.question == userQuestion && question.answer == userAnswer) {
+        if (question.question === userQuestion && question.answer === userAnswer) {
             userScores++;
+            console.log(userScores);
         }
     });
 });
