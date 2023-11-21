381f1cc1837d46ba78b496e842724571664a7d76
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-21 11:44:27 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,25 +1,22 @@
-let num1 = 10
-let num2 = num1
-
-num2 = 20;
-
-console.log('num1:', num1); // 10
-console.log('num2:', num2); // 20
 
 
+let users = [
+    { id: 1, name: 'reza', age: 20 },
+    { id: 2, name: 'ali', age: 21 },
+    { id: 3, name: 'mohsen', age: 22 },
+    { id: 4, name: 'sara', age: 23 }
+]
+
+var userData = users.find(function (user) {
+    return user.name === 'mohsen'
+})
+var userData2 = users.filter(function (user) {
+    return user.name === 'mohsen'
+})
 
 
-
-let nums1 = [1,2,3,4]
-let nums2 = nums1
-
-num2=[5,6,7,8]
-nums2.push(5)
-
-console.log('nums1:', nums1); // 1 , 2 , 3 , 4
-console.log('nums2:', nums2); // 5 , 6 , 7 , 8
-
-
+console.log("Output of find function : ", userData);
+console.log("Output of filter function : ", userData2);
 
 
 
