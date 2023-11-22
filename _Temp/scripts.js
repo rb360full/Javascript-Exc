a364a894b350dfdb21db90e7a060cde3081ce645
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-22 18:10:07 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,9 +1,28 @@
 
 
 
-const num;
 
-console.log(num); // SyntaxError: Missing initializer in const declaration
+
+
+
+
+
+
+
+
+
+
+<button class="btn btn-primary" onclick="btnClick()">Click me</button>
+
+function btnClick() {
+    let h1Elem = document.getElementById('title')
+    h1Elem.style.color = 'red'
+}
+
+
+
+
+
 
 
 
