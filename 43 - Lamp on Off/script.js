3ea00aff19c567274d0aacff59df319caf6e4310
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-22 19:43:50 +0330

    # 43 - Lamp on Off

--- 43 - Lamp on Off/script.js
+++ 43 - Lamp on Off/script.js
@@ -1,14 +1,28 @@
 
 let lampImage = document.getElementById('image')
 let lampFlag = false
+let lampButton = document.querySelector('button')
 function ChangeImage() {
     if (lampFlag) {
         lampImage.setAttribute('src', 'img/lamp_off.png')
         lampFlag = false
+        lampButton.innerText = 'Turn On'
+        lampButton.classList.add('btn-warning')
+        lampButton.classList.remove('btn-dark')
+       
+
     }
 
     else {
         lampImage.setAttribute('src', 'img/lamp_on.png')
         lampFlag = true
+        lampButton.innerText = 'Turn Off'
+        lampButton.classList.remove('btn-warning')
+        lampButton.classList.add('btn-dark')
+       
     }
 }
+
+
+
+
