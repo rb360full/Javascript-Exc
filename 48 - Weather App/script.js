897ea4a7f95c501fa89bb8fcfb4a821db72365e0
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-25 20:15:38 +0330

    # 48 - Weather App

--- 48 - Weather App/script.js
+++ 48 - Weather App/script.js
@@ -1,69 +1,67 @@
-let backImg = ['pic_1.jpg', 'pic_2.jpg', 'pic_3.jpg', 'pic_4.jpg', 'pic_5.jpg', 'pic_6.jpg', 'pic_7.jpg', 'pic_8.jpg'];
+let backImg = ["pic_1.jpg", "pic_2.jpg", "pic_3.jpg", "pic_4.jpg", "pic_5.jpg", "pic_6.jpg", "pic_7.jpg", "pic_8.jpg"];
 let backImgRandIndex = Math.floor(Math.random() * backImg.length);
 backImgRand = backImg[backImgRandIndex];
 
 function ChangeBackground() {
-    document.body.style.backgroundImage = `url(img/${backImgRand})`
+    document.body.style.backgroundImage = `url(img/${backImgRand})`;
+}
 
-};
-
-(ChangeBackground());
-
+ChangeBackground();
 
 let isCityFound = false;
-let inputCityElem = document.getElementById('inputCity');
-let searchBtnElem = document.getElementById('searchBtn');
-let searchBoxElem = document.getElementById('searchbox');
+let inputCityElem = document.getElementById("inputCity");
+let searchBtnElem = document.getElementById("searchBtn");
+let searchBoxElem = document.getElementById("searchbox");
 
-let degreeElem = document.getElementById('degree');
-let sunIconElem = document.getElementById('sunicon');
-let sunElem = document.getElementById('sun');
-let humidityElem = document.getElementById('humidity');
-let windSpeedElem = document.getElementById('windSpeed');
-let cityNameElem = document.getElementById('cityName');
+let cityNameElem = document.getElementById("cityName");
+let degreeElem = document.getElementById("degree");
+let sunIconElem = document.getElementById("sunicon");
+let sunElem = document.getElementById("sun");
+let humidityElem = document.getElementById("humidity");
+let windSpeedElem = document.getElementById("windSpeed");
 
 let citiesData = [
-    { name: 'tehran', degree: '25°C', sun: 'Sunny', humidity: '26%', windSpeed: '10 km/h' },
-    { name: 'mashhad', degree: '28°C', sun: 'Cloudy', humidity: '31%', windSpeed: '23 km/h' },
-    { name: 'shiraz', degree: '35°C', sun: 'Semi-Cloudy', humidity: '37%', windSpeed: '35 km/h' },
-    { name: 'tabriz', degree: '20°C', sun: 'Rainy', humidity: '18%', windSpeed: '26 km/h' },
-    { name: 'abadan', degree: '45°C', sun: 'Sunny', humidity: '36%', windSpeed: '15 km/h' }
-]
+    { name: "tehran", degree: "25°C", sun: "Sunny", humidity: "26%", windSpeed: "10 km/h", iconClass: "bi-brightness-high-fill" },
+    { name: "mashhad", degree: "28°C", sun: "Cloudy", humidity: "31%", windSpeed: "23 km/h", iconClass: "bi-cloud-fill" },
+    { name: "shiraz", degree: "35°C", sun: "Semi-Cloudy", humidity: "37%", windSpeed: "35 km/h", iconClass: "bi-cloud-sun" },
+    { name: "tabriz", degree: "20°C", sun: "Rainy", humidity: "18%", windSpeed: "26 km/h", iconClass: "bi-cloud-rain-fill" },
+    { name: "abadan", degree: "45°C", sun: "Sunny", humidity: "36%", windSpeed: "15 km/h", iconClass: "bi-brightness-high-fill" },
+];
 
-let unknownCity = { name: 'unknown', degree: '- °C', sun: '-', humidity: '- %', windSpeed: '- km/h' }
+let unknownCity = { name: "unknown", degree: "- °C", sun: "-", humidity: "- %", windSpeed: "- km/h" };
 
-searchBtnElem.addEventListener('click', function () {
+searchBtnElem.addEventListener("click", function () {
     let cityFounded = citiesData.find(function (cityData) {
         if (cityData.name === inputCityElem.value) {
-            isCityFound = true
+            isCityFound = true;
             backImgRandIndex = Math.floor(Math.random() * backImg.length);
             backImgRand = backImg[backImgRandIndex];
             console.log(backImgRandIndex);
-            document.body.style.backgroundImage = `url(img/${backImgRand})`
+            document.body.style.backgroundImage = `url(img/${backImgRand})`;
 
-            searchBoxElem.style.backgroundColor = 'rgba(12, 2, 2, 0.8)';
-            searchBtnElem.style.backgroundColor = 'rgba(12, 2, 2, 0.4)';
-            inputCityElem.style.backgroundColor = 'rgba(12, 2, 2, 0.2)';
+            searchBoxElem.style.backgroundColor = "rgba(12, 2, 2, 0.8)";
+            searchBtnElem.style.backgroundColor = "rgba(12, 2, 2, 0.4)";
+            inputCityElem.style.backgroundColor = "rgba(12, 2, 2, 0.2)";
 
-
-
-            return true
+            return true;
+        } else {
+            isCityFound = false;
         }
-        else {
-            isCityFound = false
+    });
 
-        }
-    })
-
-    if (isCityFound == false) { cityFounded = unknownCity }
-    else {
-
-    }
     let cityName = cityFounded.name.charAt(0).toUpperCase() + cityFounded.name.slice(1);
+
+    if (isCityFound == false) {
+        cityFounded = unknownCity;
+    } else {
+        cityNameElem.innerHTML = "Weather in " + cityName;
+        degreeElem.innerHTML = cityFounded.degree;
+        sunElem.innerHTML = cityFounded.sun;
+        sunIconElem.setAttribute("class", "");
+        sunIconElem.classList.add("bi", "text-white", cityFounded.iconClass);
+        humidityElem.innerHTML = cityFounded.humidity;
+        windSpeedElem.innerHTML = cityFounded.windSpeed;
+    }
+
     console.log(cityName);
-
-
-
-
-
-})
+});
