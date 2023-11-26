b3e640080b41934dbf2155faf48481e01ec88e1a
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-27 00:54:54 +0330

    #

--- 49 - Temp Conversion/js/script.js
+++ 49 - Temp Conversion/js/script.js
@@ -7,9 +7,14 @@ let celSpanElem = document.querySelector(".C");
 let farSpanElem = document.querySelector(".F");
 let resultElem = document.querySelector(".result");
 
+let result;
 let convertedValue;
+let resultColor;
 
-changeBtnElem.addEventListener("click", () => {
+
+
+
+function Swap() {
     let temp = farSpanElem.innerHTML;
     farSpanElem.innerHTML = celSpanElem.innerHTML;
     convInputElem.setAttribute("placeholder", temp);
@@ -18,33 +23,30 @@ changeBtnElem.addEventListener("click", () => {
     if (convInputElem.value != "") {
         CalcConvert();
     }
-});
+}
 
-convBtnElem.addEventListener("click", CalcConvert);
-resetBtnElem.addEventListener("click", Reset);
-
-function CalcConvert() {
-    let result;
+let CalcConvert = function () {
     if (convInputElem.getAttribute("placeholder") == "°C" && convInputElem.value != "") {
         convertedValue = convInputElem.value * (9 / 5) + 32;
         result = convInputElem.value + celSpanElem.innerHTML + " to " + convertedValue.toFixed(2) + farSpanElem.innerHTML;
-        resultElem.style.color = 'rgb(255, 255, 102)'
+        resultColor = "rgb(255, 255, 102)";
     } else if (convInputElem.getAttribute("placeholder") == "°F" && convInputElem.value != "") {
         convertedValue = (convInputElem.value - 32) * (5 / 9);
         result = convInputElem.value + celSpanElem.innerHTML + " to " + convertedValue.toFixed(2) + farSpanElem.innerHTML;
-        resultElem.style.color = 'rgb(255, 255, 102)'
+        resultColor = "rgb(255, 255, 102)";
     }
 
-    if (isNaN(convertedValue)) {
-        result = "Invalid Input";
-        resultElem.style.color = 'red'
-    }
     if (convInputElem.value == "") {
         result = "Enter a Value please ...";
-        resultElem.style.color = 'red'
+        resultColor = "red";
+    }
+    else if (isNaN(convertedValue)) {
+        result = "Invalid Input";
+        resultColor = "red";
     }
 
-    resultElem.innerHTML = result;
+
+    return { result:result , color: resultColor}
 }
 
 function Reset() {
@@ -54,5 +56,18 @@ function Reset() {
     resultElem.innerHTML = "";
     convInputElem.setAttribute("placeholder", "°C");
     document.title = "| " + "°C" + " to " + "°F" + " |";
-    resultElem.style.color = 'rgb(255, 255, 102)'
+    resultElem.style.color = "rgb(255, 255, 102)";
 }
+
+
+
+
+function InsertDomElements() {
+    resultElem.style.color = CalcConvert().color
+    resultElem.innerHTML = CalcConvert().result;
+
+}
+
+changeBtnElem.addEventListener("click", Swap);
+convBtnElem.addEventListener("click", CalcConvert && InsertDomElements);
+resetBtnElem.addEventListener("click", Reset);
