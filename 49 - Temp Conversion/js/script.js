5d0cdb4b4d82a142c419400e3385c84d305cb22d
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-26 15:38:35 +0330

    # 49 - Temp Conversion

--- 49 - Temp Conversion/js/script.js
+++ 49 - Temp Conversion/js/script.js
@@ -1,1 +1,53 @@
-﻿
+﻿// define variables of the HTML elements
+let convInputElem = document.getElementById("converter");
+let convBtnElem = document.querySelector(".convertButton");
+let resetBtnElem = document.querySelector(".resetButton");
+let changeBtnElem = document.querySelector(".changeButton");
+let celSpanElem = document.querySelector(".C");
+let farSpanElem = document.querySelector(".F");
+let resultElem = document.querySelector(".result");
+
+let convertedValue;
+
+changeBtnElem.addEventListener("click", () => {
+    let temp = farSpanElem.innerHTML;
+    farSpanElem.innerHTML = celSpanElem.innerHTML;
+    convInputElem.setAttribute("placeholder", temp);
+    celSpanElem.innerHTML = temp;
+    document.title = "| " + temp + " to " + farSpanElem.innerHTML + " |";
+    if (convInputElem.value != "") {
+        CalcConvert();
+    }
+});
+
+convBtnElem.addEventListener("click", CalcConvert);
+resetBtnElem.addEventListener("click", Reset);
+
+function CalcConvert() {
+    let result;
+    if (convInputElem.getAttribute("placeholder") == "°C" && convInputElem.value != "") {
+        convertedValue = convInputElem.value * (9 / 5) + 32;
+        result = convInputElem.value + celSpanElem.innerHTML + " to " + convertedValue.toFixed(2) + farSpanElem.innerHTML;
+    } else if (convInputElem.getAttribute("placeholder") == "°F" && convInputElem.value != "") {
+        convertedValue = (convInputElem.value - 32) * (5 / 9);
+        result = convInputElem.value + celSpanElem.innerHTML + " to " + convertedValue.toFixed(2) + farSpanElem.innerHTML;
+    }
+
+    if (isNaN(convertedValue)) {
+        result = "Invalid Input";
+    }
+    if (convInputElem.value == "") {
+        result = "Enter a Value please ...";
+    }
+
+    resultElem.innerHTML = result;
+}
+
+function Reset() {
+    convInputElem.value = "";
+    celSpanElem.innerHTML = "°C";
+    farSpanElem.innerHTML = "°F";
+    resultElem.innerHTML = "";
+    convInputElem.setAttribute("placeholder", "°C");
+    document.title = "| " + "°C" + " to " + "°F" + " |";
+}
