1af78a43302cccbe64d207ee900e078db19d3e35
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-27 19:24:47 +0330

    # 50 - Key Code

--- 50 - Key Code/js/script.js
+++ 50 - Key Code/js/script.js
@@ -1,11 +1,22 @@
-﻿let $ = document
-let locationKey = $.querySelector('#location')
-let title = $.querySelector('title')
+﻿let locationElem = document.getElementById('location')
+let titleElem = document.querySelector('title')
+let keyCodeElem = document.getElementById('keyCode')
+let keyElem = document.getElementById('key')
+let whichElem = document.getElementById('which')
+let codeElem = document.getElementById('code')
+
 
 document.body.addEventListener('keydown', function (event) {
-	
+	event.preventDefault();
 	starter.style.display = 'none'
 	heading.style.display = 'flex'
 	ascii.style.display = 'flex'
 	infos.style.display = 'flex'
+	keyCodeElem.innerHTML = event.keyCode
+	keyElem.innerHTML = event.key
+	locationElem.innerHTML= event.location
+	whichElem.innerHTML= event.which
+	codeElem.innerHTML= event.code
+
+
 })
