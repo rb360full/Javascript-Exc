8aa3afd4906e809745349ccfc3553ecc7b446a79
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-28 14:28:48 +0330

    #

---
+++ 51 - Password showing/js/script.js
@@ -0,0 +1,24 @@
+﻿ // Functions for select elems with id & class
+ var $ = document;
+ function _id(id_name) {
+   return $.getElementById(id_name);
+ }
+
+ function _class(class_name) {
+   return $.getElementsByClassName(class_name);
+ }
+
+ // Select Elems
+ var togglePassword = _class("toggle-password");
+ var passwordField = _id("password-field");
+
+ // Fire click event on eye icon
+ togglePassword.addEventListener("click", function () {
+   if (passwordField.type == "text") {
+     passwordField.type = "pasword";
+     togglePassword.classList.remove("active");
+   } else {
+     passwordField.type = "text";
+     togglePassword.classList.add("active");
+   }
+ })
