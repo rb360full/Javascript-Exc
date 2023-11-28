bd08715f421a8dde7ec42920c44d5fd04906ee18
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-29 02:43:18 +0330

    #

--- 52 - Todo List/js/script.js
+++ 52 - Todo List/js/script.js
@@ -1,1 +1,37 @@
-﻿
+﻿let inputTodo = document.getElementById("inputTodo");
+let icons = document.querySelectorAll('i');
+let ulElem = document.querySelector("ul");
+
+
+icons.forEach(function (icon) {
+    icon.addEventListener('click', function (event) {
+        icon.parentElement.remove()
+    })
+})
+
+inputTodo.addEventListener('keydown', function (event) {
+    if (event.key == 'Enter') {
+        event.preventDefault();
+        let newLiElem = document.createElement('li');
+        newLiElem.className = "list-group-item d-flex justify-content-between align-items-center"
+        let newSpanElem = document.createElement('span')
+
+        newIconElem = document.createElement('i');
+        newIconElem.className = "fa fa-trash-o delete";
+        newLiElem.appendChild(newSpanElem);
+        newLiElem.appendChild(newIconElem);
+        newSpanElem.innerText = inputTodo.value;
+        ulElem.appendChild(newLiElem)
+
+        icons = document.querySelectorAll('i');
+        inputTodo.value = ''
+
+
+        icons.forEach(function (icon) {
+            icon.addEventListener('click', function (event) {
+                icon.parentElement.remove()
+            })
+        })
+
+    }
+})
--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,19 +1,42 @@
+let ul = document.querySelector('.list');
+
+let lis = document.querySelectorAll('.list-item');
+
+let input = document.querySelector('#input');
+let spans = document.querySelectorAll('.list-item');
 
 
+input.addEventListener('keydown', (event) => {
+    
+    if (event.key == 'Enter') {
+        event.preventDefault();
+        let li = document.createElement('li');
+        li.classList.add('list-item');
+        let span= document.createElement('span');
+        li.appendChild(span);
+        span.innerHTML = input.value;
+        ul.appendChild(li);
+        spans = document.querySelectorAll('.list-item');
+        lis = document.querySelectorAll('.list-item');
+        input.value = '';
+        console.log(lis);
+        lis.forEach(li => {
 
-
-
-
-let listItems = document.querySelectorAll("li");
-
-listItems.forEach(function (li) {
-    li.addEventListener("pointerover", function (event) {
-        console.log("pp");
-        event.target.remove();
-    })
-
+            li.addEventListener('click', () => {
+                li.remove();
+            })
+        })
+        
+    }
 })
 
+
+spans.forEach(span => {
+
+    span.addEventListener('click', (event) => {
+        event.target.parentElement.remove();
+    })
+})
 
 
 
