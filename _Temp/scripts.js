53ec7d8402049d4ae463d29176671508b86d494c
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-29 18:19:34 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -2,8 +2,14 @@ let ul = document.querySelector('.list');
 
 let lis = document.querySelectorAll('.list-item');
 
-let input = document.querySelector('#input');
+let input = document.getElementById('input');
 let spans = document.querySelectorAll('.list-item'); 
 
 
 
+spans.forEach(function (span) {
+    span.addEventListener('click', function () {
+        input.focus();
+        span.classList.toggle('done');
+    });
+})
