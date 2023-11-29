66f39b645857b6290a7307cbcc14dd3b2358eb7c
Reza Bazdaran <rbazdaran@gmail.com>
2023-11-29 18:13:09 +0330

    # declare del icons function

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -3,7 +3,7 @@ let ul = document.querySelector('.list');
 let lis = document.querySelectorAll('.list-item');
 
 let input = document.querySelector('#input');
-let spans = document.querySelectorAll('.list-item');
+let spans = document.querySelectorAll('.list-item'); 
 
 
 
