4ac32cd1c8d502c4ba08d7dbe45d4d8281532cb4
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-01 02:50:36 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -14,37 +14,27 @@ let lis = _classes("list-item");
 let input = _id("input");
 let button = _id("button");
 
-let keys = Object.keys(localStorage);
+let localKeys = Object.keys(localStorage);
 let keyValueArray = [];
-
-for (let key of keys) {
-    let value = JSON.parse(localStorage.getItem(key));
-    console.log(value.inputVal);
-
-    let inputVal = value.inputVal;
-
-    keyValueArray.push({ key, value });
+for (let key of localKeys) {
+    let data = JSON.parse(localStorage.getItem(key));
+    let objectLocal = { key: key, value: data.value, date: data.date };
+    keyValueArray.push(objectLocal);
 }
-
 keyValueArray.sort(function (a, b) {
-    return a.value.date - b.value.date;
+    return a.date - b.date;
+});
+
+keyValueArray.forEach(function (item) {
+    ul.innerHTML += `<li class="list-item"><span>${item.value}</span></li>`;
 });
 
 console.log(keyValueArray);
 
-keyValueArray.forEach(function (obj) {
-    let element = `<li class="list-item"><span>${obj.value.inputVal}</span></li>`;
-    ul.innerHTML += element;
-});
-
 button.addEventListener("click", function () {
-    console.log(input.value);
-    let inputVal = input.value;
+    ul.innerHTML += `<li class="list-item"><span>${input.value}</span></li>`;
     let date = new Date().getTime();
-    console.log(date);
-    let data = { inputVal, date };
-
-    localStorage.setItem(`value${input.value}`, JSON.stringify(data));
-    let element = `<li class="list-item"><span>${input.value}</span></li>`;
-    ul.innerHTML += element;
+    let inputVal = input.value;
+    let data = { value: inputVal, date: date };
+    localStorage.setItem(`todoItem${date}`, JSON.stringify(data));
 });
