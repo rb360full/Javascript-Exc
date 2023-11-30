7b36dbd89b9339d198db9cd882d30036138dfb26
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-01 00:58:28 +0330

    # 52 - Todo List

--- 52 - Todo List/js/script2.js
+++ 52 - Todo List/js/script2.js
@@ -1,43 +1,31 @@
 // declare variables and functions
-let $ = document
-function _id(elemId = "") { return $.getElementById(elemId) }
-function _class(elemClass = "") { return $.querySelector("." + elemClass) }
-function _classes(elemClasses = "") { return $.querySelectorAll("." + elemClasses) }
-let idCounter = localStorage.getItem('idCounter')
-let idCounters = []
-if (localStorage.getItem('idCounters') == undefined) {
-    localStorage.setItem('idCounters', 0)
+let $ = document;
+function _id(elemId = "") {
+    return $.getElementById(elemId);
 }
-else {
-    idCounters = localStorage.getItem('idCounters').split(',').map(Number)
-    console.log(idCounters);
+function _class(elemClass = "") {
+    return $.querySelector("." + elemClass);
 }
-
-
-
-
-let myData = 0;
-
-
+function _classes(elemClasses = "") {
+    return $.querySelectorAll("." + elemClasses);
+}
+let idCounter = localStorage.getItem("idCounter");
 
 function timeNow() {
-    const currentTime = new Date()
+    const currentTime = new Date();
     const hours = currentTime.getHours();
     const minutes = currentTime.getMinutes();
     const seconds = currentTime.getSeconds();
-    return (hours + ':' + minutes + ':' + seconds);
+    return hours + ":" + minutes + ":" + seconds;
 }
-
 
 function removeTodo(id = "") {
     _id(id).parentElement.parentElement.remove();
-
 }
 
 function addTodo(value = "") {
-    idCounter++
-    let output =
-        `
+    idCounter++;
+    let output = `
     <li class=" list-group-item d-flex justify-content-between align-items-center">
     <span>${value}</span>
     <span>
@@ -45,95 +33,46 @@ function addTodo(value = "") {
       <i class="fa fa-pencil px-3 edit" id="edit${idCounter}"></i>
       <i class="fa fa-trash-o delete" id="del${idCounter}"></i>
     </span>
-  </li>`
-    localStorage.setItem(`item${idCounter}`, output)
-    _id('ultodo').innerHTML += localStorage.getItem(`item${idCounter}`)
-    // idCounters = [concat.(localStorage.getItem('idCounters'))]
-    localStorage.setItem('idCounter', idCounter)
-
-    idCounters.push(idCounter)
-    localStorage.setItem('idCounters', idCounters)
-    console.log(idCounters);
-
-    return output
-
+  </li>`;
+    _id("ultodo").innerHTML += output;
+    return output;
 }
-
 
 function editTodo(id = "", value = "") {
-    _id(id).parentElement.previousElementSibling.innerHTML = value
+    _id(id).parentElement.previousElementSibling.innerHTML = value;
 }
 
-////////////////////////////////////
-
-
-for (let i = 0; i < idCounters.length; i++) {
-    _id('ultodo').innerHTML += localStorage.getItem(`item${idCounters[i]}`)
-}
-
-
-//  localItems = localStorage.getItem(`item${i}`)
-//  _id('ultodo').innerHTML += localItems
-
-
-
-
-_id('inputtodo').addEventListener('keydown', function (event) {
-
-
+_id("inputtodo").addEventListener("keydown", function (event) {
     if (event.key == "Enter") {
-        event.preventDefault()
+        event.preventDefault();
 
-        addTodo(_id('inputtodo').value)
+        addTodo(_id("inputtodo").value);
 
+        _id("inputtodo").value = "";
 
+        _classes("delete").forEach(function (item) {
+            item.addEventListener("click", function (event) {
+                removeTodo(item.id);
 
+                localStorage.removeItem(`item${item.id.slice(-1)}`);
+            });
+        });
 
-
-
-
-
-        _id('inputtodo').value = ""
-
-
-        _classes('delete').forEach(function (item) {
-            item.addEventListener('click', function (event) {
-                removeTodo(item.id)
-
-                localStorage.removeItem(`item${item.id.slice(-1)}`)
-            })
-        })
-
-
-        _classes('edit').forEach(function (item) {
-            item.addEventListener('click', function (event) {
-                editTodo(item.id, _id('inputtodo').value)
-            })
-        })
-
-
+        _classes("edit").forEach(function (item) {
+            item.addEventListener("click", function (event) {
+                editTodo(item.id, _id("inputtodo").value);
+            });
+        });
     }
-
-
-
-
-
-})
-
-
+});
 
 ///////////////////////////////
 
-_classes('edit').forEach(function (item) {
-    item.addEventListener('click', function (event) {
-        editTodo(item.id, _id('inputtodo').value)
-    })
-})
-
-
-
-
-
+_classes("edit").forEach(function (item) {
+    item.addEventListener("click", function (event) {
+        editTodo(item.id, _id("inputtodo").value);
+    });
+});
 
 // _classes('edit').forEach(function (item) {
 //     item.addEventListener('click', function (event) {
@@ -141,4 +80,3 @@ _classes('edit').forEach(function (item) {
 //         console.log(item.id);
 //     })
 // })
-
--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,53 +1,53 @@
-let ul = document.querySelector('.list');
+let $ = document;
+function _id(elemId = "") {
+    return $.getElementById(elemId);
+}
+function _class(elemClass = "") {
+    return $.querySelector("." + elemClass);
+}
+function _classes(elemClasses = "") {
+    return $.querySelectorAll("." + elemClasses);
+}
 
-let lis = document.querySelectorAll('.list-item');
+let ul = _class("list");
+let lis = _classes("list-item");
+let input = _id("input");
+let button = _id("button");
 
-let input = document.getElementById('input');
-let spans = document.querySelectorAll('.list-item');
-
-
-const element1 = document.querySelector('[idReza="reza"]');
-
-console.log(element1);
-
-
-
-let i = 0
-
-
-
-
-
-spans.forEach(function (span) {
-    span.addEventListener('click', function (event) {
-        i++
-        input.focus();
-        input.value = span.textContent
-        span.setAttribute('idReza', i)
+let keys = Object.keys(localStorage);
+let keyValueArray = [];
 
 
-        input.addEventListener('keydown', function (event) {
-            if (event.key == 'Enter') {
+for (const key of keys) {
+    const value = JSON.parse(localStorage.getItem(key));
+    console.log(value.inputVal);
 
-                (document.querySelector(`[idReza="${i}"]`)).innerHTML = input.value
+    let inputVal = value.inputVal
 
+    keyValueArray.push({ key, value });
+}
 
-
-            }
-            
-        })
-
-
-
-    });
+keyValueArray.sort(function (a, b) {
+    return a.value.date - b.value.date
     
-})
+});
 
+console.log(keyValueArray);
 
+keyValueArray.forEach(function (obj) {
+    let element = `<li class="list-item"><span>${obj.value.inputVal}</span></li>`;
+    ul.innerHTML += element;
+});
 
-input.addEventListener('keydown' , function(event){
-    if (event.key == 'Enter'){
-        document.querySelector('.list').innerHTML +=`<li class="list-item"><span>${input.value}</span></li>`
-    }
+button.addEventListener("click", function () {
+    console.log(input.value);
+    let inputVal = input.value;
+    let date = new Date().getTime()
+    console.log(date);
+    let data = { inputVal, date };
 
-})
+    localStorage.setItem(`value${input.value}`, JSON.stringify(data));
+    let element = `<li class="list-item"><span>${input.value}</span></li>`;
+    ul.innerHTML += element;
+});
+
