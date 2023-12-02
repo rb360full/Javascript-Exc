c305d7dc2210e3619c65566945fe7f31ca82da21
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-02 18:36:12 +0330

    # 53 - Custom Context Menu

--- 53 - Custom Context Menu/js/script.js
+++ 53 - Custom Context Menu/js/script.js
@@ -1,1 +1,17 @@
-﻿
+﻿const contextMenu = document.getElementById("contextMenu");
+document.body.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    contextMenu.style.display = "block";
+    contextMenu.style.top = `${e.pageY}px`;
+    contextMenu.style.left = `${e.pageX}px`;
+});
+
+document.body.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        contextMenu.style.display = "none";
+    }
+});
+
+document.body.addEventListener("click", (e) => {
+    contextMenu.style.display = "none";
+});
