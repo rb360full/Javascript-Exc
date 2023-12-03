3acfbbb6393581929dfae4ecea4db11b445698db
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-03 22:28:07 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,19 +1,6 @@
 const box = document.getElementById('box');
 
-function handleMouseMove() {
-  console.log('moved');
-}
-
-function removeMouseMoveListener() {
-  box.removeEventListener('mousemove', handleMouseMove);
-  console.log('removed');
-}
-
-box.addEventListener('click', removeMouseMoveListener);
-box.addEventListener('mousemove', handleMouseMove);
-
-
-document.body.addEventListener('touchcancel',function(){
-  console.log('touch Canceled');
+document.addEventListener('scroll', function (e) {
+  console.log(document.documentElement.scrollTop, document.documentElement.scrollIntoView, document.documentElement.scrollWidth);
 })
 
