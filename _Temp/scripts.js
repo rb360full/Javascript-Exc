40454163f21d4ea72ef7b6b0fddbe84386fed063
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-04 15:30:04 +0330

    # music player

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -2,36 +2,26 @@
 
 
 
+const audioElem = document.querySelector('audio')
+const playBtn = document.getElementById('play')
+const pauseBtn = document.getElementById('pause')
+const durationBtn = document.getElementById('duration')
+const speed2XBtn = document.getElementById('speed2x')
+const currentTimeBtn = document.getElementById('current')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const divElem = document.querySelector('div')
-
-divElem.addEventListener('click', function (event) {
-  console.log(event.target.dataset);
+playBtn.addEventListener('click', function (e) {
+  audioElem.play()
+})
+pauseBtn.addEventListener('click', function (e) {
+  audioElem.pause()
+})
+durationBtn.addEventListener('click', function (e) {
+  console.log(audioElem.duration);
 })
 
-
-
-
-
-
-
-
+speed2XBtn.addEventListener('click', function (e) {
+  audioElem.playbackRate = 2
+})
+currentTimeBtn.addEventListener('click', function (e) {
+  console.log(audioElem.currentTime);
+})
