78833f23e93ae1c29964d95dca9d6a632e534abc
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-06 01:37:24 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,27 +1,4 @@
-
-
-
-
-const audioElem = document.querySelector('audio')
-const playBtn = document.getElementById('play')
-const pauseBtn = document.getElementById('pause')
-const durationBtn = document.getElementById('duration')
-const speed2XBtn = document.getElementById('speed2x')
-const currentTimeBtn = document.getElementById('current')
-
-playBtn.addEventListener('click', function (e) {
-  audioElem.play()
-})
-pauseBtn.addEventListener('click', function (e) {
-  audioElem.pause()
-})
-durationBtn.addEventListener('click', function (e) {
-  console.log(audioElem.duration);
-})
-
-speed2XBtn.addEventListener('click', function (e) {
-  audioElem.playbackRate = 2
-})
-currentTimeBtn.addEventListener('click', function (e) {
-  console.log(audioElem.currentTime);
-})
+if (true) {
+  
+  
+}
