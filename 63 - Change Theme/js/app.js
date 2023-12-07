151db41affb714d78a4e4d09148fe62f50561094
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-07 13:19:41 +0330

    # 63 - Change Theme

--- 63 - Change Theme/js/app.js
+++ 63 - Change Theme/js/app.js
@@ -1,5 +1,21 @@
-const switchElement = document.querySelector('.switch')
+const body = document.body;
+const switchElement = document.querySelector(".switch");
 
-switchElement.addEventListener('click', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  let currentTheme = localStorage.getItem("theme");
+  body.classList.add(currentTheme);
+});
+
+switchElement.addEventListener("click", function () {
   // Hint: Add 'dark' class to body :))
-})
+
+  let currentTheme = localStorage.getItem("theme");
+
+  if (currentTheme == "light" || currentTheme == null) {
+    localStorage.setItem("theme", "dark");
+    body.classList.toggle("dark");
+  } else if (currentTheme == "dark") {
+    localStorage.setItem("theme", "light");
+    body.classList.toggle("dark");
+  }
+});
