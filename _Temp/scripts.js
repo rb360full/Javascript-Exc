fb985dcbedf28297d57fbe664400b5a3d9fa84b6
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-17 10:07:05 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -16,4 +16,15 @@ console.log(myDate.toISOString());
 
 
 
- 
+ let myUsersList = [
+    {id:1 , name:'reza' , family:'bazdaran', age:20},
+    {id:2 , name:'mohammad' , family:'bazdaran', age:20},
+    {id:3 , name:'ali' , family:'bazdaran', age:20},
+    {id:4 , name:'saeed' , family:'bazdaran', age:20}  
+ ]
+
+
+ myUsersList.push(myUsersList)
+ console.log(myUsersList)
+
+ let newUser = {id:5 , name:'akbar' ,  }
