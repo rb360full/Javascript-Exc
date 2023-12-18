8a4f6666441c3d94adac9be557665c60d7a2fc31
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-19 01:13:04 +0330

    # online shop

--- 74 - Shopping Basket/js/store.js
+++ 74 - Shopping Basket/js/store.js
@@ -4,9 +4,11 @@ const shopItemsContainer = document.querySelector(".shop-items");
 const cart = document.querySelector(".cart-items");
 const cartTotalPrice = document.querySelector(".cart-total-price");
 const cartQuantity = document.querySelector(".cart-quantity-input");
+const purchaseBtn = document.querySelector(".btn-purchase");
+const removeElem = document.querySelector(".btn.btn-danger");
 
 // variables
-let products = [
+const products = [
     { id: "product-1", name: "Album 1", price: 11.99, imageSrc: "Images/Album 1.png", quantity: 1 },
     { id: "product-2", name: "Album 2", price: 12.99, imageSrc: "Images/Album 2.png", quantity: 1 },
     { id: "product-3", name: "Album 3", price: 13.99, imageSrc: "Images/Album 3.png", quantity: 1 },
@@ -14,7 +16,7 @@ let products = [
     { id: "product-5", name: "T Shirt", price: 25.99, imageSrc: "Images/Shirt.png", quantity: 1 },
     { id: "product-6", name: "Band Members", price: 36.99, imageSrc: "Images/Band Members.png", quantity: 1 },
 ];
-let cardProducts = [];
+let cartProducts = [];
 
 // Functions
 
@@ -37,11 +39,11 @@ function appendProducts(products) {
 }
 
 function addToCart(product) {
-    cardProducts.push(product);
+    cartProducts.push(product);
 
     cart.innerHTML = "";
 
-    cardProducts.forEach((product) => {
+    cartProducts.forEach((product) => {
         const productItem = `
             <div class="cart-row" id="${product.id}">
                <div class="cart-item cart-column">
@@ -50,7 +52,7 @@ function addToCart(product) {
                </div>
                <span class="cart-price cart-column">$${product.price}</span>
                <div class="cart-quantity cart-column">
-                   <input class="cart-quantity-input" type="number" value=${product.quantity}>
+                   <input class="cart-quantity-input" type="number" min="1" value=${product.quantity} >
                    <button class="btn btn-danger" type="button">REMOVE</button>
                </div>
              </div>`;
@@ -59,18 +61,35 @@ function addToCart(product) {
     });
 }
 
-function totalPrice(cardProducts) {
+function totalPrice(cartProducts) {
     let cardTotalPrice = 0;
     let productSum = 0;
-    cardProducts.forEach((product) => {
+    cartProducts.forEach((product) => {
         productSum = product.quantity * product.price;
 
         cardTotalPrice += productSum;
     });
-
     cartTotalPrice.innerHTML = cardTotalPrice.toFixed(2);
 
     return cardTotalPrice;
+}
+
+function purchase() {
+    cartProducts = [];
+    cart.innerHTML = "";
+}
+
+function removeCardItem(event) {
+    if (event.target.innerHTML === "REMOVE") {
+        const product = event.target.closest("div.cart-row");
+        let productIndex = cartProducts.findIndex((prod) => {
+            return prod.id == product.id;
+        });
+
+        cartProducts[productIndex].quantity = 1;
+        cartProducts.splice(productIndex, 1);
+        product.remove();
+    }
 }
 
 // Events
@@ -82,21 +101,32 @@ shopItemsContainer.addEventListener("click", function (e) {
             return product.id == targetProduct.id;
         });
 
-        addToCart(findedProduct);
-        totalPrice(cardProducts);
+        !cartProducts.includes(findedProduct) ? addToCart(findedProduct) : null;
+        totalPrice(cartProducts);
     }
 });
 
 cart.addEventListener("change", function (e) {
     if (e.target.className.includes("cart-quantity-input")) {
         const targetProduct = e.target.closest("div.cart-row");
-        let findedProduct = products.find((product) => {
+        let findedProduct = cartProducts.find((product) => {
             return product.id == targetProduct.id;
         });
 
         findedProduct.quantity = e.target.value;
-        totalPrice(cardProducts);
+
+        totalPrice(cartProducts);
     }
+});
+
+purchaseBtn.addEventListener("click", () => {
+    purchase();
+    totalPrice(cartProducts);
+});
+
+cart.addEventListener("click", (event) => {
+    removeCardItem(event);
+    totalPrice(cartProducts);
 });
 
 // Calls
