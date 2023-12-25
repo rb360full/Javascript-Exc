4676ee06d9ca139ccd7f249ac16b27260c4ac43b
Reza Bazdaran <rbazdaran@gmail.com>
2023-12-25 15:01:30 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,14 +1,19 @@
-const ulElem = document.querySelector('ul')
-
-let users = ['reza','ali','mosi','hasan']
-
-let usersFragment = document.createDocumentFragment()
-
-users.forEach((user)=>{
-    let newLi = document.createElement('li')
-    newLi.innerText = user
-    usersFragment.appendChild(newLi)
-})
 
 
-ulElem.appendChild(usersFragment)
+let user = {
+    id: 1,
+    name: 'reza',
+    family: 'bazdaran',
+    age: 40
+}
+
+
+let products = [
+    { id: 1, name: 'phone', price: 12000000, color: 'black', count: 15 },
+    { id: 2, name: 'laptop', price: 32000000, color: 'blue', count: 18 },
+    { id: 3, name: 'tablet', price: 54000000, color: 'red', count: 25 },
+    { id: 4, name: 'mouse', price: 6500000, color: 'green', count: 16 }
+]
+
+console.table(products);
+console.table(user);
