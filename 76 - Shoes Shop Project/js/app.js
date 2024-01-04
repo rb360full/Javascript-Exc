f0c71a118ddd0f4110e2369fdbccbe0584a633d3
Reza Bazdaran <rbazdaran@gmail.com>
2024-01-04 22:37:12 +0330

    # Shoes Products

--- 76 - Shoes Shop Project/js/app.js
+++ 76 - Shoes Shop Project/js/app.js
@@ -1,2 +1,24 @@
-// DOM => Document Object Model
-// BOM => Browser Object Model
+const containerElem = document.querySelector('.container')
+
+const products = [
+    { id: 1, name: 'Sport Shoes', price: 200, description: 'This is  the description of the product', imageSrc: 'images/1.png' },
+    { id: 2, name: 'Women Shoes', price: 100, description: 'This is  the description of the product', imageSrc: 'images/2.png' },
+    { id: 3, name: 'Boots', price: 150, description: 'This is  the description of the product', imageSrc: 'images/3.png' }
+]
+
+
+
+products.forEach(product => {
+
+    containerElem.insertAdjacentHTML("afterbegin", `
+    <div class="product-card">
+        <h1>${product.name}</h1>
+        <p>${product.description}</p>
+        <div class="product-pic" style="background-image: url(${product.imageSrc});"></div>
+        <div class="product-info">
+          <div class="product-price">$${product.price}</div>
+          <a href="product.html?id=${product.id}" class="product-button">See More</a>
+        </div>
+      </div>
+    `)
+})
--- 76 - Shoes Shop Project/js/product.js
+++ 76 - Shoes Shop Project/js/product.js
@@ -1,1 +1,41 @@
-// ✌ :))
+// Elements
+const backButton = document.querySelector('button')
+
+// Data and variables
+const products = [
+    { id: 1, name: 'Sport Shoes', price: 200, description: 'This is  the description of the product', imageSrc: 'images/1.png' },
+    { id: 2, name: 'Women Shoes', price: 100, description: 'This is  the description of the product', imageSrc: 'images/2.png' },
+    { id: 3, name: 'Boots', price: 150, description: 'This is  the description of the product', imageSrc: 'images/3.png' }
+]
+
+const searchParam = new URLSearchParams(window.location.search)
+const productId = searchParam.get('id')
+
+
+let product = products.find(p => {
+    return p.id == productId
+})
+
+
+
+document.body.insertAdjacentHTML('beforeend', `
+<div class="details">
+<div class="desc">
+  <h1>${product.name}</h1>
+  <p>
+    ${product.description}
+    ${product.description}
+    ${product.description}
+  </p>
+</div>
+<div class="image">
+  <img src="${product.imageSrc}" />
+</div>
+</div>
+`)
+
+
+// Events
+backButton.addEventListener('click', () => {
+    window.history.back()
+})
