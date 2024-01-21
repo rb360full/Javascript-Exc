15980312867ec97d26ae3ec58d1329449ca6d91f
Reza Bazdaran <rbazdaran@gmail.com>
2024-01-22 00:08:26 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,7 +1,7 @@
-
-num = 12
-
-'use strict'
-
-console.log(num);
+window.addEventListener('load', () => {
+    let dbSet = indexedDB.open('temp')
+    dbSet.addEventListener('success', e => console.log('Success :', e))
+    dbSet.addEventListener('error', e => console.log('Error :', e))
+    dbSet.addEventListener('upgradeneeded', e => console.log('Upgrade :', e))
+})
 
