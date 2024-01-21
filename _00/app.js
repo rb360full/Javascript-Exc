cdb383575e050c3440b4183d1fa94ab0ea952389
Reza Bazdaran <rbazdaran@gmail.com>
2024-01-21 08:16:42 +0330

    #

---
+++ _00/app.js
@@ -0,0 +1,5 @@
+import * as funcs from './funcs.js'
+
+
+funcs.logger()
+
---
+++ _00/funcs.js
@@ -0,0 +1,9 @@
+function logger() {
+    console.log('reza');
+}
+
+
+let sum = (a, b) => a + b
+
+
+export { logger, sum }
