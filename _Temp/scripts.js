294823e564dd2c0f798e947d8f4f910597df2ce8
Reza Bazdaran <rbazdaran@gmail.com>
2024-02-12 23:11:58 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,15 +1,24 @@
 
 
-document.addEventListener('scroll', () => {
-    let scrollTop = document.documentElement.scrollTop
-    let clientHeight = document.documentElement.clientHeight
-    let scrollHeight = document.documentElement.scrollHeight
-  
-    let scroll = (scrollTop / (scrollHeight - clientHeight)) * 100;
+let addCdnLink = (cdn) => {
+    let newLinkTag = document.createElement("link");
+    newLinkTag.href = cdn;
+    newLinkTag.rel = "stylesheet";
+    document.head.appendChild(newLinkTag);
 
-    console.log('scroll : ', scroll + '%');
-})
+    return new Promise((resolved, reject) => {
+        newLinkTag.onload = () => resolved("File Attached");
+        newLinkTag.onerror = () => reject("File Not Attached");
+    });
+};
 
 
+
+
+console.log(addCdnLink('style.css'));
+
+// addCdnLink("style.css")
+//     .then((message) => console.log(message))
+//     .catch((message) => console.log(message));
 
 
