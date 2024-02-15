273c0cf6faf5fc943ea185711f237ca1ac971e5d
Reza Bazdaran <rbazdaran@gmail.com>
2024-02-15 10:31:22 +0330

    #

---
+++ 84 - Weather app/content/js/app.js
@@ -0,0 +1,13 @@
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+
+    const myApi = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=49e59b4d3b43e99c74710be5a95c3eeb'
+
+
+    // https://api.openweathermap.org/data/2.5/weather?q=iran&appid=49e59b4d3b43e99c74710be5a95c3eeb
+
+    
--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,25 +1,2 @@
-
-
-let addCdnLink = (cdn) => {
-    let newLinkTag = document.createElement("link");
-    newLinkTag.href = cdn;
-    newLinkTag.rel = "stylesheet";
-    document.head.appendChild(newLinkTag);
-
-    return new Promise((resolved, reject) => {
-        newLinkTag.onload = () => resolved("File Attached");
-        newLinkTag.onerror = () => reject("File Not Attached");
-    });
-};
-
-
-
-
-console.log(addCdnLink('style.css'));
-
-// addCdnLink("style.css")
-//     .then((message) => console.log(message))
-//     .catch((message) => console.log(message));
-
-
-
+fetch('https://jsonplaceholder.typicode.com/posts/15')
+    .then(res => res.json()).then(res => console.log(res))
