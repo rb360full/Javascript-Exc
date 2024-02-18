77e2e709692025839b96c1138c1fb68d8c4c053b
Reza Bazdaran <rbazdaran@gmail.com>
2024-02-18 15:49:26 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,69 +1,9 @@
 
-class Users {
-    constructor(firstName, lastName, age, job) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.age = age
-        this.job = job
-    }
 
-    startDev = () => {
-        console.log(`${this.firstName} start Developping...`);
-    }
-    getAge(){
-        return this.age
-    }
-}
-
-let rezaUser = new Users('reza', 'bazdaran', 40, 'web developer');
-
-rezaUser.startDev()
+// window.navigator.clipboard.writeText('salam reza , chetori ?')
 
 
+window.addEventListener('click',e=>{
 
-
-
-class Teachers extends Users {
-
-    constructor(firstName, lastName, age, job, lessonTeaching) {
-        super(firstName, lastName, age, job)
-        this.lessonTeaching = lessonTeaching
-    }
-
-    startDev(){
-        console.log('ok');
-        console.log(`${this.firstName} teacher start teaching...`);
-    }
-}
-
-
-let myTeacher = new Teachers('mohammad', 'rezaei', 50, 'web developer', 'html,css,js')
-
-console.log(myTeacher);
-
-myTeacher.startDev()
-
-console.log(myTeacher.getAge());
-
-class TodoTask {
-    constructor(id,taskName,taskDateStart,taskDateEnd) {
-        this.id = id
-        this.taskName = taskName
-        this.taskDateStart = taskDateStart
-        this.taskDateEnd = taskDateEnd
-    }
-    
-    addTask(){
-        console.log('ok');
-        console.log(`${this.taskName} task added...`);
-
-    }
-}
-
-
-
-let newTask = new TodoTask(1,'reading book','2021-01-01','2021-01-10')
-
-newTask.addTask()
-
-console.log(newTask);
+    console.log(window.navigator.clipboard.readText());
+})
