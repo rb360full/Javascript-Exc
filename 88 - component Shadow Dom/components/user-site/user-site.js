2729976b4453905ff8ae06c4f9fc98e4aee46ca4
Reza Bazdaran <rbazdaran@gmail.com>
2024-02-24 11:47:14 +0330

    #

--- 88 - component Shadow Dom/components/user-site/user-site.js
+++ 88 - component Shadow Dom/components/user-site/user-site.js
@@ -1,14 +1,15 @@
 let template = document.createElement("template");
 
 template.innerHTML = `
-<slot name='username'></slot>
+<link rel="stylesheet" href="components/user-site/user-Site.css">
+ <slot name='username'></slot>
+<h1></h1>
 <slot name='email'></slot>
 <slot name='job'></slot>
 <slot class="pc" name='pc'></slot>
 <slot name="remove"></slot>
 
-<link rel="stylesheet" href="components/user-site/user-Site.css">
-`
+`;
 
 class UserSite extends HTMLElement {
     constructor() {
@@ -18,42 +19,35 @@ class UserSite extends HTMLElement {
     }
 
     removeComponent() {
-        this.remove()
+        this.remove();
     }
 
     connectedCallback() {
-        let removeBtn = this.shadowRoot.querySelectorAll('slot')[4]
-        
-         removeBtn.addEventListener('click', ()=>{
-            this.removeComponent()
-        })
+        let removeBtn = this.shadowRoot.querySelectorAll("slot")[4];
+        let userSiteElement = this.shadowRoot.querySelector('h1')
+        userSiteElement.innerHTML = this.getAttribute('user-site')
 
-
-
-
-
-
-
+        removeBtn.addEventListener("click", () => {
+            this.removeComponent();
+        });
     }
 
     disconnectedCallback() {
-        let removeBtn = this.shadowRoot.querySelectorAll('slot')[4]
-        removeBtn.removeEventListener('click', ()=>{
-            
-            this.removeComponent()
-        })
+        let removeBtn = this.shadowRoot.querySelectorAll("slot")[4];
+        removeBtn.removeEventListener("click", () => {
+            this.removeComponent();
+        });
     }
 
 
+    static observedAttributes() {
 
+        return ['user-site']
+
+    }
 }
 
-export { UserSite }
-
-
-
-
-
+export { UserSite };
 
 // site-user.js
 // class UserSite extends HTMLElement {
