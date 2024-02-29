85f84bfedf187aa19fd13da8a28f790fc190695b
Reza Bazdaran <rbazdaran@gmail.com>
2024-02-29 12:44:16 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -1,64 +1,25 @@
-// //////////////////////////// Callback Hell ////////////////////////////
+async function f1(...args) {
+  let sum = 0;
 
-// const do1 = (fname, lname, callback) => {
-//     setTimeout(() => {
-//         const user = {
-//             fname,
-//             lname,
-//         };
-//         console.log("the user is : ", user);
-//         do2();
-//     }, 2000);
-// };
+  let promise = new Promise((resolve) => {
+    setTimeout(() => {
+      for (let arg of args) {
+        sum += arg;
+      }
+      resolve(sum);
+    }, 2000);
+  });
 
-// const do2 = (course) => {
-//     let lessons = { course };
-//     let out = setTimeout(() => {
-//         console.log("the course is : ", lessons);
-//     }, 1000);
-//     return out;
-// };
+  return promise;
+}
 
-// do1("reza", "bazdaran");
-// // do2('JavaScript')
+async function runCodes(...args) {
+  let lg = await f1(...args);
+  console.log(lg);
+}
 
-//////////////////////////// Promise ////////////////////////////
+runCodes(1, 2, 3);
 
-
-//////////////////////////// Callback Hell ////////////////////////////
-
-// const do1 = (fname, lname) => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve({
-//                 fname,
-//                 lname
-
-
-//             })
-
-//         }, 2000);
-//     })
-// };
-
-// const do2 = (course) => {
-//     return new Promise((resolve, reject) => {
-
-//         setTimeout(() => {
-//             resolve({ 'this is the course': course })
-//         }, 1000);
-//     })
-// };
-
-
-// do1('reza', 'bazdaran').then(user => {
-//     console.log(user);
-// }).then(res => {
-//     do2('javascript').then(course => {
-//         console.log(course);
-//     })
-// })
-
-
-//////////////////////////// Async Await ////////////////////////////
-
+f1(1, 2, 3, 4).then((res) => {
+  console.log(res);
+});
