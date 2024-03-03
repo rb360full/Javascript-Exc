e095ebb9b8574e578033d7b0de66cb92185e1378
Reza Bazdaran <rbazdaran@gmail.com>
2024-03-03 21:25:55 +0330

    #

--- _Temp/scripts.js
+++ _Temp/scripts.js
@@ -13,13 +13,15 @@ async function f1(...args) {
   return promise;
 }
 
-async function runCodes(...args) {
+(async function runCodes(...args) {
   let lg = await f1(...args);
   console.log(lg);
-}
+}())
 
-runCodes(1, 2, 3);
+// runCodes(1, 2, 3);
 
 f1(1, 2, 3, 4).then((res) => {
   console.log(res);
 });
+
+//
